1ea32f7ca06a2b457f84666ff406f77a89c834f5
KaveeN <vpdkkaveen@gmail.com>
2025-02-21 14:23:27 +0530

    added OrderDetails entity

--- src/main/java/lk/ijse/gdse/supermarket/entity/OrderDetails.java
+++ src/main/java/lk/ijse/gdse/supermarket/entity/OrderDetails.java
@@ -1,12 +1,12 @@
 package lk.ijse.gdse.supermarket.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.Date;
 
 /**
  * --------------------------------------------
@@ -22,16 +22,10 @@ import java.util.Date;
 @Getter
 @Setter
 @Entity
-@Table(name = "orders")
-public class Order {
+@Table(name = "order_details")
+public class OrderDetails {
     @Id
-    @Column(name = "order_id")
     private String id;
 
-    @Column(name = "order_date")
-    private Date orderDate;
 
-    @ManyToOne
-    @JoinColumn(name = "customer_id")
-    private Customer customer;
 }
